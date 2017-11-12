65a5fd7d49b59d8fb31605690c18940262ed199c
yangjianbo <149516374@qq.com>
2017-11-12 22:34:57 +0800

    删除病例功能

--- pkuhit.iih.ui/src/sy/patient/action/DelMrDocAction.js
+++ pkuhit.iih.ui/src/sy/patient/action/DelMrDocAction.js
@@ -3,21 +3,20 @@ Ext.define('iih.sy.patient.action.DelMrDocAction', {
     
     doExecute: function(context) {
     	var self=this;
-//    	var owner = self.getOwner();
 		var result=self.getBlock('result');
 		var mrTree=result.down("xapgrid");
-		var treeNode = mrTree.getSelectionModel().getSelection();		
+		var treeNode = mrTree.getSelectionModel().getSelection();
 		var mr=treeNode[0].raw;
         var data={
-            	mrPk:mr.mrPk,
-            	encounterPk:mr.enPk,
-            	updateCount:mr.updateCount
-            };
-		self.prepareOperations(context.operations,data);
+        	mrPk:mr.mrPk,
+        	encounterPk:mr.enPk,
+        	updateCount:mr.updateCount
+        };
+        self.prepareOperations(context.operations,data);
 	},
     prepareOperations: function(operations,datas) {
         var url = this.url;
-        url += '/' + datas.mrPk;       
+        url += '/' + datas.mrPk; 
         var mclass = null;
         var METHODS = this.getInvocationMethods();
         var operation = {
@@ -33,7 +32,7 @@ Ext.define('iih.sy.patient.action.DelMrDocAction', {
     },
     onSuccess: function(operation) {
         var owner = this.getOwner();
-    	XapMessageBox.info('删除成功');
+    	XapMessageBox.info('删除成功!');
         var init = owner.getActionChain('init');
         if(init) {
         	init.execute();
